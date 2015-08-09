7e07eee04278cf2d254f48a0d4e13b5d7299aa79
aziz-ka <hasanov.aziz@gmail.com>
2015-08-09 18:23:26 -0400

    added side section; finished profile tab

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+(function() {
+	$(".tab-content").height(function() {
+		return $(window).height() - 75;
+	});
+	$(".last-li").height(function() {
+		return $(window).height() - $(".list-group").height();
+	});
+})();

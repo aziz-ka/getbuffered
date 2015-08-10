b4f35bd5ebe8e11375ec5c895f66308a3f34d0b1
aziz-ka <hasanov.aziz@gmail.com>
2015-08-10 01:00:14 -0400

    finished skills and projects tabs; added modal on page load; disabled responsiveness; changed favicon

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 (function() {
+	var isBuffer = sessionStorage.getItem("buffer");
+	
+	if(!isBuffer) {
+		$("#entry-form").modal("show");
+		$(".alert").hide();
+		$("#company").change(function(event) {
+			var companyPick = event.target.value;
+			if(companyPick === "buffer") {
+				$(".alert-danger").hide();
+				$(".alert-success").show();
+
+				setTimeout(function() {
+					$("#entry-form").modal("hide");
+				}, 2000);
+				sessionStorage.setItem("buffer", true);
+			}
+			if(companyPick === "other") {
+				$(".alert-success").hide();
+				$(".alert-danger").show();
+			}
+			if(companyPick === "none") {
+				$(".alert").hide();
+			}
+		})
+	} else {
+		$("#entry-form").modal("hide");
+	}
+	
 	$(".tab-content").height(function() {
 		return $(window).height() - 75;
 	});
